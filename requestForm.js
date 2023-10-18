dd2aa3b98c12afba61da749bd0c8e27903f62fbc
zakky4 <sadaoza@gmail.com>
2023-10-18 10:31:43 +0900

    フォーム送信からChatwork処理部分を切り出し

--- requestForm.js
+++ requestForm.js
@@ -5,33 +5,17 @@
 // フォームのデータを送信
 function requestForm(e) {
   const values = e.values;
+
+  // 登録日を取得
+  const created = Utilities.formatDate(
+    new Date(values[0]),
+    "JST",
+    "yyyy-MM-dd HH:mm"
+  );
+
   // データをNotionに登録
   createPage(createPayload(values));
 
   // Chatworkにメッセージを送信
-  const cw_token = chatworkToken();
-
-  // ルームID
-  let room_id = "";
-  if (values[5] === "はい") {
-    // 【code4biz】サポート用
-    room_id = "338413933";
-  } else {
-    // 【code4biz】営業用
-    room_id = "338413911";
-  }
-
-  // メッセージ生成
-  const message = `${values[1]}\n
-${values[2]}\n
-${values[3]}\n
-${values[4]}\n
-${values[5]}\n
-${values[6]}\n`;
-
-  const client = ChatWorkClient.factory({ token: cw_token });
-  client.sendMessage({
-    room_id: room_id,
-    body: message,
-  });
+  sendChatwork(values[5], values, created);
 }
